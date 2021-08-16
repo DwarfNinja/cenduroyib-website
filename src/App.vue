77c7af34b2629a04bae516d7cc7660da25508cb9
DwarfNinja <cenduroyib@gmail.com>
2021-08-16 16:38:35 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Header></Header>
+</template>
+
+<script setup>
+import Header from "./components/Header.vue";
+
+</script>
+
+<style>
+body {
+  @apply bg-custom-blue
+}
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul id="header-menu">
+    <li class="header-menu-item"><a href="">Home</a></li>
+    <li class="header-menu-item"><a href="">Skills</a></li>
+    <li class="header-menu-item"><a href="">Services</a></li>
+    <li class="header-menu-item"><a href="">Contact</a></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+
+#header-menu {
+  @apply bg-white
+}
+
+.header-menu-item {
+  @apply text-black
+}
+
+</style>
