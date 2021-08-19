86010bd987315540bf8faab8b9d9ff22da9b0fc4
DwarfNinja <cenduroyib@gmail.com>
2021-08-19 22:21:35 +0200

    Added explicit asset imports for Vite build.
    Added explicit import for assets in main.js  for Vite build.

--- src/main.js
+++ src/main.js
@@ -3,4 +3,13 @@ import App from './App.vue';
 import './tailwind.css';
 import './vanillatilt-custom.js';
 
+import javalogo from "./assets/java-logo.png";
+import pythonlogo from "./assets/python-logo.png";
+import gitlogo from "./assets/git-logo.png";
+import godotlogo from "./assets/godot-logo.png";
+import jsonlogo from "./assets/json-logo.png";
+import javascriptlogo from "./assets/javascript-logo.png";
+import htmllogo from "./assets/html-logo.png";
+import csslogo from "./assets/css-logo.png";
+
 createApp(App).mount('#app')
