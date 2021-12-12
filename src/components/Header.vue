364181116ec5fe6b8d384789b9f2bf0837ca1454
DwarfNinja <cenduroyib@gmail.com>
2021-12-12 20:05:19 +0100

    Restyled Header Ver1.0

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,13 @@
 <template>
-  <header class="flex bg-white h-20 justify-center items-center shadow-customb px-9">
-    <nav>
-      <ul id="header-menu">
-        <li class="header-menu-item "><a href="#title">Home</a></li>
-        <li class="header-menu-item"><a href="#skills">Skills</a></li>
-        <li class="header-menu-item"><a href="#services">Services</a></li>
-        <li class="header-menu-item"><a href="#contact">Contact</a></li>
-        <li class="header-menu-item"><a href="#about-website">About</a></li>
-      </ul>
+  <header class="flex justify-center w-full h-16 top-2 sm:top-4 fixed z-10">
+    <nav class="flex bg-white w-full max-w-3xl justify-center shadow-customb rounded-2xl">
+        <ul id="header-menu" class="flex flex-wrap gap-y-2 justify-center m-auto">
+          <li><a class="header-menu-item" href="#title">Home</a></li>
+          <li><a class="header-menu-item"  href="#skills">Skills</a></li>
+          <li><a class="header-menu-item"  href="#services">Services</a></li>
+          <li><a class="header-menu-item"  href="#contact">Contact</a></li>
+          <li><a class="header-menu-item"  href="#about-website">About</a></li>
+        </ul>
     </nav>
   </header>
 </template>
@@ -21,13 +21,14 @@ export default {
 <style scoped>
 
 .header-menu-item {
-  @apply text-black m-2 text-lg inline-block;
+  @apply mx-1 sm:mx-4 text-sm sm:text-lg p-1 px-2 rounded-2xl bg-customlightpurple;
 }
 
 .header-menu-item:hover {
-  text-decoration-line: underline ;
-  text-underline-offset: 0.3rem;
-  text-decoration-thickness: 2px;
+  @apply bg-customdarkpurple;
+  /*text-decoration-line: underline ;*/
+  /*text-underline-offset: 0.3rem;*/
+  /*text-decoration-thickness: 2px;*/
 }
 
 </style>
