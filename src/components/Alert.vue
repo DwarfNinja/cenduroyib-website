eb9049d77d1c9edf11e351a49c87382e6bee695d
DwarfNinja <cenduroyib@gmail.com>
2022-06-29 16:55:39 +0200

    Improved Alert animation and code

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="`fixed bg-${alertDataDic[formresponse].colour} p-3 sm:p-5 top-5 left-1/20 sm:left-auto right-1/20 sm:right-2 rounded z-10`">
+  <div :class="`fixed ${alertDataDic[formresponse].colour} p-3 sm:p-5 top-5 left-1/20 sm:left-auto right-1/20 sm:right-2 rounded z-10`">
     <div class=""></div>
     <span :class="`${alertDataDic[formresponse].icon} mr-2`"></span>
     <span id="alert-message">{{ alertDataDic[formresponse].message }}</span>
@@ -15,12 +15,12 @@ export default {
       alertDataDic: {
         success: {
           message: "Thank you, your message has been sent!",
-          colour: "customgreen",
+          colour: "bg-customgreen",
           icon: "fas fa-check-circle"
         },
         fail: {
           message: "Sorry there was a problem, your message could not be sent!",
-          colour: "customred",
+          colour: "bg-customred",
           icon: "fas fa-times-circle"
         }
       }
@@ -32,30 +32,30 @@ export default {
 <style scoped>
 
 div {
-  animation: slideDown 0.6s ease-in-out;
+  animation: slideDown 0.6s ease-in-out forwards, slideDown 0.6s 4s ease-in-out reverse forwards;
 }
 
 @media (min-width: 640px) {
   div {
-    animation: slideLeft 0.6s ease-in-out;
+    animation: slideLeft 0.6s ease-in-out forwards, slideLeft 0.6s 4s ease-in-out reverse forwards;
   }
 }
 
 @keyframes slideLeft {
   0% {
-    transform: translateX(450px);
+    transform: translateX(110%);
   }
   100% {
-    transform: translateX(0px);
+    transform: translateX(0%);
   }
 }
 
 @keyframes slideDown {
   0% {
-    transform: translateY(-450px);
+    transform: translateY(-130%);
   }
   100% {
-    transform: translateY(0px);
+    transform: translateY(0%);
   }
 }
 
