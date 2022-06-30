c6da41545df4ddb461e4337c5b9222d132bb2b8b
DwarfNinja <cenduroyib@gmail.com>
2022-06-30 15:25:36 +0200

    Feat: improve responsiveness of skill cards

--- src/components/SkillCard.vue
+++ src/components/SkillCard.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="w-36 sm:w-44 h-40 sm:h-48 m-2 sm:m-3" style="transform-style: preserve-3d;">
+  <div class="w-36 lgph:w-[10.5rem] sm:w-44 h-40 lgph:h-44 sm:h-48 m-2 sm:m-3" style="transform-style: preserve-3d;">
     <div class="skillcard-front skillcard-face">
       <p class="mb-2 font-semibold">{{ skill.name }}</p>
-      <img class="w-10/12 sm:w-11/12 h-auto m-auto" style="transform: translateZ(20px);" :src="`/${skill.imgsrc}`" :alt="`${skill.name} Logo`">
+      <img class="w-[6.5rem] lgph:w-10/12 sm:w-11/12 h-auto m-auto object-cover" style="transform: translateZ(20px);" :src="`/${skill.imgsrc}`" :alt="`${skill.name} Logo`">
     </div>
     <div class="skillcard-back skillcard-face flex justify-between flex-col">
       <p class="font-semibold">{{ skill.name }}</p>
