17362abe7a166ac8117e893c1fdd72a64dd88187
DwarfNinja <cenduroyib@gmail.com>
2022-07-01 00:00:47 +0200

    Feat!: fully overhaul Skills
    Add new Skills
    Split skills into two categories
    Add toggle to choose categories
    Improve device responsiveness of SkillCards
    Add skill level meter to SkillCards
    Improve code, use vue more effectively

--- src/components/SkillCard.vue
+++ src/components/SkillCard.vue
@@ -2,12 +2,15 @@
   <div class="w-36 lgph:w-[10.5rem] sm:w-44 h-40 lgph:h-44 sm:h-48 m-2 sm:m-3" style="transform-style: preserve-3d;">
     <div class="skillcard-front skillcard-face">
       <p class="mb-2 font-semibold">{{ skill.name }}</p>
-      <img class="w-[6.5rem] lgph:w-10/12 sm:w-11/12 h-auto m-auto object-cover" style="transform: translateZ(20px);" :src="`/${skill.imgsrc}`" :alt="`${skill.name} Logo`">
+      <img class="w-[105px] lgph:w-[120px] sm:w-[140px] h-auto m-auto object-cover p-1" style="transform: translateZ(20px);" :src="`/${skill.imgsrc}`" :alt="`${skill.name} Logo`">
     </div>
     <div class="skillcard-back skillcard-face flex justify-between flex-col">
       <p class="font-semibold">{{ skill.name }}</p>
-      <p class="text-sm sm:text-base" style="transform: translateZ(10px);">{{ skill.text }}</p>
-      <p class="text-sm" style="transform: translateZ(10px);">Experience: <br> {{ skill.exp }}</p>
+      <p class="text-sm sm:text-base" style="transform: translateZ(10px);">Experience: <br> {{ skill.exp }}</p>
+      <p class="text-sm sm:text-base" style="transform: translateZ(10px);">Skill level:</p>
+      <div class="w-full bg-gray-300 rounded-2xl">
+        <div :class="skill.level" class="text-right py-2 bg-customyellow text-white rounded-2xl"></div>
+      </div>
     </div>
   </div>
 </template>
