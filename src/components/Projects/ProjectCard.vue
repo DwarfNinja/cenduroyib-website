94ad7d9bd6f0d12738dd80304178c902f039ff5c
DwarfNinja <cenduroyib@gmail.com>
2022-07-27 17:11:32 +0200

    Feat: add projects section
    Replace Services section with Projects section

---
+++ src/components/Projects/ProjectCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="relative m-2 hover:scale-110 transition-transform ease-out duration-200 group">
+    <img class="w-[260px] lgph:w-[275px] md:w-[290px]" style="transform: translateZ(20px);" :src="`/${project.imgsrc}`" :alt="`${project.name} Logo`">
+    <div class="absolute top-0 bottom-0 left-0 right-0 bg-black opacity-40 md:opacity-0 group-hover:opacity-60 transition-opacity ease-out duration-200"></div>
+    <div class="absolute top-1/2 left-1/2 w-full transform -translate-x-1/2 -translate-y-1/2 p-3 font-bold text-xl sm:text-2xl text-white
+    opacity-100 md:opacity-0 group-hover:opacity-100 transition-opacity ease-out duration-200">
+      <h2 class="text-xl sm:text-2xl">{{ project.name }}</h2>
+      <h3 class="text-base sm:text-lg">{{ project.desc }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProjectCard",
+  props: ["project"]
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Projects/Projects.vue
@@ -0,0 +1,67 @@
+<template>
+  <section class="bg-white text-black w-full md:w-[93%] py-16 mx-auto mb-52 sm:max-w-8xl shadow-custombr" id="projects">
+    <h2 class="text-2xl mb-12 underline-custom">
+      Projects
+    </h2>
+      <div class="flex justify-start align-top h-full px-5 md:px-12 lg:px-16">
+        <div class="flex flex-wrap mx-auto mb-auto justify-center" style="grid-template-columns: repeat(auto-fit, minmax(9rem, 1fr));">
+          <ProjectCard :project="projects.loopsnelheid"></ProjectCard>
+          <ProjectCard :project="projects.cloakandhammer"></ProjectCard>
+          <ProjectCard :project="projects.blackjack"></ProjectCard>
+          <ProjectCard :project="projects.lingo"></ProjectCard>
+          <ProjectCard :project="projects.tetris"></ProjectCard>
+          <ProjectCard :project="projects.cenduroyib"></ProjectCard>
+        </div>
+      </div>
+  </section>
+</template>
+
+<script>
+import ProjectCard from "./ProjectCard.vue";
+
+export default {
+  name: "Projects",
+  components: {ProjectCard},
+  data() {
+    return {
+      toggleValue: true,
+      projects: {
+        loopsnelheid: {
+          name: "Loopsnelheid",
+          imgsrc: "loopsnelheid-app.png",
+          desc: "Mobile App For Tracking Walkingspeed",
+        },
+        cloakandhammer: {
+          name: "Cloak And Hammer",
+          imgsrc: "cloakandhammer-game.png",
+          desc: "Personal Game Project",
+        },
+        blackjack: {
+          name: "Blackjack",
+          imgsrc: "blackjack-api.png",
+          desc: "Blackjack API",
+        },
+        lingo: {
+          name: "Lingo",
+          imgsrc: "lingo-api.png",
+          desc: "Lingo API",
+        },
+        tetris: {
+          name: "Tetris",
+          imgsrc: "tetris-game.png",
+          desc: "Single And Multiplayer Tetris Game",
+        },
+        cenduroyib: {
+          name: "CendurOyib",
+          imgsrc: "cenduroyib-website.png",
+          desc: "Personal Website",
+        },
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
