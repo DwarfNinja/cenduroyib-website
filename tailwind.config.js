2b8d0a67eb34ea2543e1649d0b5dc5f2c5aa2117
DwarfNinja <cenduroyib@gmail.com>
2022-12-08 13:59:36 +0100

    Build: update all dependencies
    Update Vue, Vite, TailwindCSS and others to latest version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 const { screens } = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  mode: "jit",
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     screens: {
       "lgph": "420px",
@@ -34,11 +32,6 @@ module.exports = {
         "3": "3px"
       }
     }
-  },
-  variants: {
-    extend: {
-      borderWidth: ["focus"]
-    },
   },
   plugins: [],
 }
