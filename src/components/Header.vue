82ff8a00bcb7837c9512afc643269121535ec0f6
DwarfNinja <cenduroyib@gmail.com>
2022-12-20 01:01:30 +0100

    Fix: navbar layout on mobile and tablet

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
-  <header class="flex justify-center w-full h-16 top-2 sm:top-4 fixed z-10">
-    <nav class="flex bg-white w-full max-w-3xl justify-center shadow-customb rounded-2xl">
-        <ul id="header-menu" class="flex flex-wrap gap-y-2 justify-center m-auto">
+  <header class="flex justify-center w-full top-2 sm:top-4 fixed z-10">
+    <nav class="flex">
+        <ul id="header-menu" class="flex flex-wrap gap-y-2 justify-center m-auto p-2.5 min-[490px]:p-3.5 sm:p-4 bg-white w-full max-w-3xl shadow-customb rounded-2xl">
           <li><a class="header-menu-item" href="#title">Home</a></li>
           <li><a class="header-menu-item" href="#who">Who am I?</a></li>
           <li><a class="header-menu-item"  href="#skills">Skills</a></li>
@@ -21,7 +21,7 @@ export default {
 
 <style scoped>
 .header-menu-item {
-  @apply mx-1 sm:mx-4 text-sm sm:text-lg p-1 px-2 rounded-2xl bg-customlightpurple;
+  @apply mx-1 md:mx-4 text-sm sm:text-lg p-1 px-2 rounded-2xl bg-customlightpurple;
 }
 
 .header-menu-item:hover {
