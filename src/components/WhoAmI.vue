405c3fd5c4f443ba2ee2087f3378e74f67267d39
DwarfNinja <cenduroyib@gmail.com>
2022-12-28 15:13:06 +0100

    Feat: add computed study years property
    Computes an ordinal based of start date of study
    Add method to convert number to ordinal

--- src/components/WhoAmI.vue
+++ src/components/WhoAmI.vue
@@ -6,7 +6,7 @@
       <h2 id="who-h2" class="h2-custom mb-6" data-aos="fade-up" data-aos-duration="800" data-aos-anchor-placement="center-center">Who am I?</h2>
       <p class="p-custom mx-auto px-6 sm:px-8" style="max-width: 60rem"
          data-aos="fade-up" data-aos-duration="800" data-aos-delay="500" data-aos-anchor="#who-h2" data-aos-anchor-placement="center-center">
-        Hi, my name is Cendur, I am {{ age }} years old and a third year student studying
+        Hi, my name is Cendur, I am {{ age }} years old and a {{ studyYearsOrdinal }} year student studying
         Software Development at the University of Applied Sciences in Utrecht - the Netherlands.
         Creative, cheerful and problem-solving with a passion for all things tech, my people skills help me bridge the gap
         between the tech and business world.
@@ -24,9 +24,22 @@ export default {
       let diffMs = (Date.now() - dateOfBirth);
       let age_dt = new Date(diffMs);
       return Math.abs(age_dt.getUTCFullYear() - 1970);
+    },
+
+    studyYearsOrdinal() {
+      let startOfStudy = new Date("09/01/2019");
+      let diffMs = (Date.now() - startOfStudy);
+      let study_dt = new Date(diffMs);
+      return this.numberToOrdinal(Math.abs(study_dt.getUTCFullYear() - 1970) + 1);
+    }
+  },
+  methods: {
+    numberToOrdinal(number) {
+      const ordinal = ['th', 'st', 'nd', 'rd'];
+      const v = number % 100;
+      return number + (ordinal[(v - 20) % 10] || ordinal[v] || ordinal[0]);
     }
   }
-
 }
 </script>
 
